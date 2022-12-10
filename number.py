9a3227e6aa646147a8ff39b316ea8fa948dfa3ea
Feipi <72340260+Feipi@users.noreply.github.com>
2022-12-10 21:38:47 +0800

    Create number.py
    增加检测性别、地区识别！

--- number.py
+++ number.py
@@ -4,6 +4,7 @@
 
 # 前置操作
 import datetime  # 年龄判断使用
+import json  # 读取JSON用
 
 class NumBerUse:
     def __init__(self, num):
@@ -24,8 +25,12 @@ class NumBerUse:
                 print("正确！")
                 return True
             except:
-                print("错误")
-                return False
+                # 校验码是否为X
+                if self.num[17] == "X":
+                    return True
+                else:
+                    print("错误")
+                    return False
             finally:
                 # 释放内存
                 print("完毕")
@@ -33,6 +38,24 @@ class NumBerUse:
     def showage(self):
         today = datetime.date.today()  # 今天的日期
         year = today.year  # 今年的年份
-        num_year = self.num[6:14]  # 截取到身份证中的日期
+        num_year = int(self.num[6:10])  # 截取到身份证中的日期
         age = year - num_year  # 得出年龄
         return age  # 返回年龄
+
+    # 查看性别
+    def sex(self):
+        sex_num = self.num[16]
+        # 判断男女
+        if int(sex_num) % 2 == 0:
+            return "女"
+        elif int(sex_num) % 2 == 1:
+            return "男"
+
+    # 检测地区（城市、省份）
+    def city_load(self):
+        f = open("city.json","r",encoding="utf8")
+        json_text = f.read()
+        city = json.loads(json_text)
+        city_num = self.num[:2]
+        City_china = city[city_num]
+        return City_china;
